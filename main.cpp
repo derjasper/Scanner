9c0fde4e04aa0d478a3311724cc78b3843f243f0
Jasper Nalbach <derjasper@posteo.de>
2015-12-25 19:48:48 +0100

    updated main.c

--- main.cpp
+++ main.cpp
@@ -10,9 +10,9 @@ static void drawSquare( cv::Mat& image, const std::vector<cv::Point2f>& square )
     std::vector<cv::Point> ptx;
     for (int i=0;i<square.size();i++)
         ptx.push_back(cv::Point(square[i].x,square[i].y));
-    
+
     const cv::Point * p = &ptx[0];
-    
+
     int n = (int)square.size();
     cv::polylines(image, &p, &n, 1, true, cv::Scalar(0,255,0), 3, 8);
 
@@ -21,38 +21,35 @@ static void drawSquare( cv::Mat& image, const std::vector<cv::Point2f>& square )
 
 int main(int argc, char** argv)
 {
-    static const char* names[] = {  "../data/img5.jpg", 0 };
-
-    for( int i = 0; names[i] != 0; i++ )
+    for( int i = 1; i < argc; i++ )
     {
-        cv::Mat image = cv::imread(names[i], 1);
+        cv::Mat image = cv::imread(argv[i], 1);
         if( image.empty() )
         {
-            std::cout << "Couldn't load " << names[i] << std::endl;
+            std::cout << "Couldn't load " << argv[i] << std::endl;
             continue;
         }
-        
+
         char previewName[100];
-        strcpy(previewName,names[i]);
+        strcpy(previewName,argv[i]);
         strcat(previewName," (Preview)");
-        
+
         // preview
         std::vector<cv::Point2f> square = scanner::find_square(image);
         cv::Mat copy = image.clone();
         drawSquare(copy, square);
-        
+
         cv::namedWindow( previewName, cv::WINDOW_NORMAL );
         cv::imshow(previewName, copy);
 
         // process
         cv::Mat processed = scanner::process(image,1);
-        
-        cv::namedWindow( names[i], cv::WINDOW_NORMAL );
-        cv::imshow(names[i], processed);
+
+        cv::namedWindow( argv[i], cv::WINDOW_NORMAL );
+        cv::imshow(argv[i], processed);
     }
-    
+
     cv::waitKey();
 
     return 0;
 }
-
